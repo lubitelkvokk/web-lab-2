a23969c3564bfe2263aa9c0e24334041968f6b79
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-10-17 01:19:51 +0300

    checking x,y,r

--- src/main/java/AreaCheckServlet.java
+++ src/main/java/AreaCheckServlet.java
@@ -2,6 +2,7 @@ import beans.Row;
 import beans.Table;
 import com.google.gson.Gson;
 import controller.ForwardElements;
+import exceptions.InputException;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import utils.PointRequestBody;
 
+import javax.print.attribute.standard.JobKOctets;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -26,41 +28,50 @@ public class AreaCheckServlet extends HttpServlet {
         PointRequestBody requestBody = gson.fromJson(request.getReader(), PointRequestBody.class);
 
         // Получение параметров из объекта requestBody
-        Double x = (Double) request.getAttribute("x");
-        Double y = (Double) request.getAttribute("y");
-        Double r = (Double) request.getAttribute("r");
-        String timeZone = (String) request.getAttribute("timeZone");
-        boolean isHit = isHit(x, y, r);
+        try {
+            double x = parseX(request.getAttribute("x"));
+            double y = parseY(request.getAttribute("y"));
+            double r = parseR(request.getAttribute("r"));
+//            double x = (Double) request.getAttribute("x");
+//            double y = (Double) request.getAttribute("y");
+//            double r = (Double) request.getAttribute("r");
+            String timeZone = (String) request.getAttribute("timeZone");
+            boolean isHit = isHit(x, y, r);
 
-        TimeZone tz = TimeZone.getTimeZone(timeZone);
+            TimeZone tz = TimeZone.getTimeZone(timeZone);
 
-        // Получаем текущее время в заданном часовом поясе
-        Calendar calendar = Calendar.getInstance(tz);
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        int second = calendar.get(Calendar.SECOND);
-
-
-        long endTime = System.nanoTime();
-        long scriptRuntime = (endTime - startTime);
-
-        Row lastCheck = new Row(x,
-                y,
-                r,
-                timeZone + ": " + hour + ":" + minute + ":" + second,
-                scriptRuntime, isHit);
-
-        Table table = (Table) request.getSession().getAttribute("table");
-        table.getTable().add(lastCheck);
-        request.getSession().setAttribute("table", table);
+            // Получаем текущее время в заданном часовом поясе
+            Calendar calendar = Calendar.getInstance(tz);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            int second = calendar.get(Calendar.SECOND);
 
 
-        request.getSession().setAttribute("lastCheck", lastCheck);
+            long endTime = System.nanoTime();
+            long scriptRuntime = (endTime - startTime);
 
-        response.setStatus(HttpServletResponse.SC_FOUND); // Установка кода состояния 302 (Found)
+            Row lastCheck = new Row(x,
+                    y,
+                    r,
+                    timeZone + ": " + hour + ":" + minute + ":" + second,
+                    scriptRuntime, isHit);
 
-        response.sendRedirect(request.getContextPath() + "/result");
-//        getServletContext().getRequestDispatcher("/result").forward(request, response);
+            Table table = (Table) request.getSession().getAttribute("table");
+            table.getTable().add(lastCheck);
+            request.getSession().setAttribute("table", table);
+
+
+            request.getSession().setAttribute("lastCheck", lastCheck);
+
+            response.setStatus(HttpServletResponse.SC_FOUND); // Установка кода состояния 302 (Found)
+
+            response.sendRedirect(request.getContextPath() + ForwardElements.RESULT_JSP.getName());
+        } catch (InputException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // Установка кода состояния 404
+            request.getSession().setAttribute("error_message", e.getMessage());
+            response.sendRedirect(request.getContextPath() + ForwardElements.ERROR_PAGE.getName());
+        }
+
     }
 
     private boolean isHit(Double x, Double y, Double r) {
@@ -75,6 +86,39 @@ public class AreaCheckServlet extends HttpServlet {
             isHit = true;
         }
         return isHit;
+    }
+
+    private double parseX(Object x) throws InputException {
+        double dx;
+        try {
+            dx = (Double) x;
+            if (dx > 2 || dx < -2) throw new InputException("x has an invalid format: " + dx);
+        } catch (ClassCastException | InputException e) {
+            throw new InputException("x has an invalid format");
+        }
+        return dx;
+    }
+
+    private double parseY(Object y) throws InputException {
+        double dy;
+        try {
+            dy = (Double) y;
+            if (5.0 <= dy || dy <= -5.0) throw new InputException("y has an invalid format: " + dy);
+        } catch (ClassCastException | InputException e) {
+            throw new InputException("y has an invalid format");
+        }
+        return dy;
+    }
+
+    private double parseR(Object r) throws InputException {
+        double dr;
+        try {
+            dr = (Double) r;
+            if (dr <= 2 || dr >= 5) throw new InputException("r has an invalid format");
+        } catch (ClassCastException | InputException e) {
+            throw new InputException("r has an invalid format");
+        }
+        return dr;
     }
 
 
